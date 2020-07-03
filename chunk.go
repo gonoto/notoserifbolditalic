e155ca5a14c11a04490ff59d1aaa9faea1853119
Nik <njunger@uwaterloo.ca>
2020-07-03 12:52:14 -0400

    Initial commit

---
+++ chunk.go
@@ -0,0 +1,4 @@
+package notoserifbolditalic
+
+var chunks = [][]uint64{chunk0, chunk1, chunk2, chunk3}
+const decompressedSize = 99849964
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gonoto/notoserifbolditalic
+
+go 1.14
